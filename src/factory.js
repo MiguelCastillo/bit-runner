fa7891f9bfc343039b7285b7ac1ede771f6c9a49
Miguel Castillo <manchagnu@gmail.com>
2015-03-04 18:44:08 -0500

    code clean up from `action` refactoring
    also added jsdocs and updated package.json

--- src/factory.js
+++ src/factory.js
@@ -1,6 +1,14 @@
 var _factory = {};
 var slice = Array.prototype.slice;
 
+
+/**
+ * Registers a named factory delegate so that when Factory.create is called,
+ * we know what factory needs to be called.
+ *
+ * @param {string} name - Name of the factory to register
+ * @param {function} factoryDelegate - Function to call to create instances
+ */
 function Factory(name, factoryDelegate) {
   if (_factory.hasOwnProperty(name)) {
     throw new TypeError('Task "' + name + '" is already registered');
@@ -8,6 +16,15 @@ function Factory(name, factoryDelegate) {
   _factory[name] = factoryDelegate;
 }
 
+
+/**
+ * Calls a registered factory delegate in order to create an instance of an
+ * object.
+ *
+ * @param {string} name - Name of the factory to call
+ *
+ * @returns {*} Instance created by the factory
+ */
 Factory.create = function(name) {
   var factory = _factory[name];
   if (!factory) {
@@ -15,5 +32,6 @@ Factory.create = function(name) {
   }
   return factory.apply(null, slice.call(arguments, 1));
 };
+
 
 module.exports = Factory;
--- src/task.js
+++ src/task.js
@@ -7,24 +7,28 @@ var util         = Bitloader.Utils;
 /**
  * @class
  *
+ * @classdesc
  * Task that can be executed by the task runner
  */
 function Task(name, deps, cb, parent, root) {
-  var task   = this;
-  var loader = (parent && parent._loader) || new Bitloader({}, {fetch: fetchFactory});
+  var task = this;
   root = root || this;
 
-  this._loader = loader;
-  this._src    = [];
-  this._tasks  = {};
-  this.name    = name;
-  this.init    = init.bind(this);
-  this.run     = run.bind(this);
-  this.load    = load.bind(this);
-  this.action  = action.bind(this);
+  if (root === this) {
+    this._loader = new Bitloader({}, {fetch: fetchFactory});
+    this._tasks  = {};
+    this._src    = [];
+  }
+
+  this.name   = name;
+  this.load   = load.bind(this);
+  this.action = action.bind(this);
+  this.run    = run.bind(this);
+  this._init  = init.bind(this);
+
 
   function run() {
-    init().then(exec);
+    init().then(exec, util.printError);
   }
 
   function load(src) {
@@ -38,13 +42,13 @@ function Task(name, deps, cb, parent, root) {
 
   function action(cb) {
     // This is a transform accumulator
-    loader.transform.use(cb);
+    root._loader.transform.use(cb);
     return task;
   }
 
   function exec() {
     if (root._src.length) {
-      return loader.import(root._src);
+      return root._loader.import(root._src);
     }
   }
 
@@ -53,7 +57,7 @@ function Task(name, deps, cb, parent, root) {
       if (typeof(cb) === 'function') {
         return cb.call(task, task);
       }
-    });
+    }, util.printError);
   }
 
   function buildTree() {
@@ -71,16 +75,50 @@ function Task(name, deps, cb, parent, root) {
       if (root._tasks.hasOwnProperty(name)) {
         return Bitloader.Promise.resolve();
       }
-      return (root._tasks[name] = Task.create(name, task, root)).init();
+      return (root._tasks[name] = Task.create(name, task, root))._init();
     };
   }
 }
 
 
-Task.prototype.init = function() {};
-Task.prototype.run  = function() {};
+/**
+ * Method to run the task. This is where the task tree is instantiated and the
+ * root task is executed.
+ */
+Task.prototype.run = function() {};
+
+
+/**
+ * Method to load file(s) from storage in order to put them through the
+ * transformatin pipeline.
+ *
+ * @param {string | string[]} name - Name or array of names of files to be
+ *  loaded.
+ *
+ * @returns {Task} Returns itself
+ */
 Task.prototype.load = function() {};
-Task.prototype.then = function() {};
+
+
+/**
+ * Method to register an action handler to receive data whenever there is data
+ * that needs processing.
+ *
+ * @param {function} handler - Called when there is data that needs processing.
+ *
+ * @returns {Task} Returns itself
+ */
+Task.prototype.action = function() {};
+
+
+/**
+ * Method that initializes the hierarchy of tasks so that they can be later
+ *  ran
+ *
+ * @returns {Promise} That will resolve when all the tasks have been created.
+ * @private
+ */
+Task.prototype._init = function() {};
 
 
 Task.factory = function(name, deps, cb) {
@@ -89,8 +127,10 @@ Task.factory = function(name, deps, cb) {
   });
 };
 
+
 Task.create = function(name, parent, root) {
   return factory.create(name, parent, root);
 };
+
 
 module.exports = Task;
